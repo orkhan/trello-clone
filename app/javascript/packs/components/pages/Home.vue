c999a39c2709e665b9e5c24012713c3f208a5d17
orkhan <orkhan.maharramli@gmail.com>
2018-06-09 17:16:21 +0400

    Frontend: Create Board

--- app/javascript/packs/components/pages/Home.vue
+++ app/javascript/packs/components/pages/Home.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="home-page">
-    <a href="#">Create Board</a>
+    <h2>Please type board name</h2>
+    <input type="text" v-model="name" @keyup.enter="createBoard()"/>
+    <button @click="createBoard()">Create Board</button>
   </div>
 </template>
 
 <script>
+  import {HTTP} from '../../utils/http'
   export default {
     name: 'Home',
+    data() {
+      return {
+        errors: [],
+        name: ''
+      }
+    },
     components: {
     },
     mounted(){
+    },
+    methods:{
+      createBoard(){
+        HTTP({
+          method: 'post',
+          url: '/boards.json',
+          data: {
+            board: {
+              name: this.name
+            }
+          },
+          dataType: 'json'
+        }).then(response => {
+          this.$router.push('/boards/' + response.data.uid);
+        })
+        .catch(e => {
+          this.errors.push(e)
+        });
+      }
     }
   }
 </script>
--- app/views/boards/_board.json.jbuilder
+++ app/views/boards/_board.json.jbuilder
@@ -1,4 +1,4 @@
-json.extract! board, :id, :name, :created_at, :updated_at
+json.extract! board, :id, :uid, :name, :created_at, :updated_at
 json.lists board.lists do |list|
   json.(list, :id, :name, :created_at, :updated_at, :cards)
 end
